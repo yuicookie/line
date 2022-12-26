8fec4e27b241458d0033c2b7199b3f90395408bf
yuicookie <104888283+yuicookie@users.noreply.github.com>
2022-12-27 04:02:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // Audioオブジェクトを作成
 let ad = new Audio("audio/xmas.m4a");
 
-// 連続再生ボタン
-let elem_loop = document.getElementById("bgm");
-elem_loop.addEventListener("click", function(){
-	ad.play();
-	ad.loop = true;  // ループ再生
-}, false);
 
 function active(obj) {
   //  クラスactiveがなかったら、activeを追加
@@ -19,4 +13,39 @@ function active(obj) {
       obj.nextElementSibling.style = "opacity:1";
     }, 500);
   }
+}
+
+
+// 右クリック禁止
+$("body").bind
+  ("contextmenu", function(e)
+  {return false;}
+  );
+$("body").mousedown
+  (function(e)
+  {return false;}
+  );
+
+function agree(){
+  document.getElementById("agree").style.display ="none";
+  document.getElementById("agree_check").style.display ="block";
+}
+function agree_check(){
+  document.getElementById("agree_check").style.display ="none";
+  document.getElementById("agree").style.display ="block";
+}
+function ok_btn(){
+  if(document.getElementById("agree_check").style.display == "block"){
+    document.getElementById("ok_btn").style.display ="none";
+    document.getElementById("agree_check").style.display ="none";
+    document.getElementById("kiyaku_title").style.display ="none";
+    document.getElementById("kiyaku_setumei").style.display ="none";
+    document.getElementById("kiyaku").style.display ="none";
+    document.getElementById("title").style.display ="none";
+    document.getElementById("lobby_epN01").style.display ="block";
+    // 連続再生
+let elem_loop = document.getElementById("bgm");
+	ad.play();
+	ad.loop = true;  // ループ再生
+ false};
 }
