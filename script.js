f93e474d18b77e6c18f411155a0d1ee78264474c
yuicookie <104888283+yuicookie@users.noreply.github.com>
2022-12-26 18:21:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ elem_loop.addEventListener("click", function(){
 	ad.play();
 	ad.loop = true;  // ループ再生
 }, false);
+
+function active(obj) {
+  //  クラスactiveがなかったら、activeを追加
+  if (!obj.nextElementSibling.classList.contains("active")) {
+    obj.nextElementSibling.classList.add("active");
+  } else {  //  フワッと消す時の処理
+    obj.nextElementSibling.style = "opacity:0"; //  まずopacityで見えなくする
+    setTimeout((e) => { //  0.5秒後にクラスactiveを削除
+      obj.nextElementSibling.classList.remove("active");
+      obj.nextElementSibling.style = "opacity:1";
+    }, 500);
+  }
+}
