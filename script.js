71cd9521e9acefe7fc632987e3fdfaf1a9b759d4
yuicookie <104888283+yuicookie@users.noreply.github.com>
2022-12-28 00:53:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -26,44 +26,17 @@ $("body").mousedown
   {return false;}
   );
 
-// 同意ボタンクリック
-const agree_btn = document.getElementById('agree');
-agree_btn.addEventListener('mousedown', e => {
+function agree(){
   document.getElementById("agree").style.display ="none";
-  document.getElementById("agree_black").style.display ="block";
-    console.log("押してる");
-})
-// 同意ボタン離す
-const agree_black = document.getElementById('agree_black');
-agree_black.addEventListener('mouseup', e => {
-  document.getElementById("agree_black").style.display ="none";
   document.getElementById("agree_check").style.display ="block";
-  document.getElementById("ok_btn_black").style.display ="none";
-  document.getElementById("ok_btn").style.display ="block";
-    console.log("押してない");
-});
-
-// 同意外すクリック離す
-const agree_check = document.getElementById('agree_check');
-agree_check.addEventListener('mouseup', e => {
+}
+function agree_check(){
   document.getElementById("agree_check").style.display ="none";
   document.getElementById("agree").style.display ="block";
-  document.getElementById("ok_btn").style.display ="none";
-  document.getElementById("ok_btn_black").style.display ="block";
-});
-
-// OKボタンクリック
-const ok_btn = document.getElementById('ok_btn');
-ok_btn.addEventListener('mousedown', e => {
-  document.getElementById("ok_btn").style.display ="none";
-  document.getElementById("ok_btn_black").style.display ="block";
-    console.log("押してる");
-})
-// OKボタン離す
-const ok_btn_black = document.getElementById('ok_btn_black');
-ok_btn_black.addEventListener('mouseup', e => {
+}
+function ok_btn(){
   if(document.getElementById("agree_check").style.display == "block"){
-    document.getElementById("ok_btn_black").style.display ="none";
+    document.getElementById("ok_btn").style.display ="none";
     document.getElementById("agree_check").style.display ="none";
     document.getElementById("kiyaku_title").style.display ="none";
     document.getElementById("kiyaku_setumei").style.display ="none";
@@ -74,6 +47,5 @@ ok_btn_black.addEventListener('mouseup', e => {
 let elem_loop = document.getElementById("bgm");
 	ad.play();
 	ad.loop = true;  // ループ再生
- false;
-    console.log("押してない");
-}});
+ false};
+}
